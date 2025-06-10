1c30fc4445df115158bbe8ef6ce284d1238e3886
Karol Orszulik <orkarox@gmail.com>
2025-06-10 21:28:53 +0200

    Grid

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from Simulation import Simulation
 import neat
 import random
 
-iteration = 0
+epoch = 0
 SKIP_FIRST_EPOCHS = 50
 DISPLAY_EVERY_EPOCH = 10
 
 def eval_genomes(genomes, config):
-    global iteration
+    global epoch
 
     # Reset simulation once for the whole generation
     sim.reset()
@@ -20,7 +20,7 @@ def eval_genomes(genomes, config):
         net = neat.nn.FeedForwardNetwork.create(genome, config)
         nets.append(net)
 
-    NUM_ITERATIONS = int(min(1500, 300 + (iteration / 15) * 100))
+    NUM_ITERATIONS = int(min(1500, 300 + (epoch / 15) * 100))
     # Run the simulation for all walkers in parallel
     for _ in range(NUM_ITERATIONS):
         # Get inputs for all walkers
@@ -39,14 +39,14 @@ def eval_genomes(genomes, config):
         # Update all walkers with their respective outputs
         sim.update(all_outputs)
 
-        if iteration >= SKIP_FIRST_EPOCHS and iteration % DISPLAY_EVERY_EPOCH == 0:
+        if epoch >= SKIP_FIRST_EPOCHS and epoch % DISPLAY_EVERY_EPOCH == 0:
             sim.draw()
     
     # Evaluate fitness for each genome
     for i, (genome_id, genome) in enumerate(genomes):
         genome.fitness = sim.walkers[i].fitness()
         
-    iteration += 1
+    epoch += 1
 
 if __name__ == "__main__":
     sim = Simulation()
@@ -62,4 +62,23 @@ if __name__ == "__main__":
     p.add_reporter(stats)
     # p.add_reporter(neat.Checkpointer(40))
 
-    winner = p.run(eval_genomes, 500)
+    winner = p.run(eval_genomes, 200)
+
+    # playback the best genome
+    sim.reset()
+    sim.make_walkers(1)
+    best_genome = neat.nn.FeedForwardNetwork.create(winner, config)
+    
+    while sim.running:
+        inputs = sim.walkers[0].info().as_array()
+        outputs = best_genome.activate(inputs)
+        sim.handle_events()
+        if not sim.running:
+            exit(1)
+        
+        sim.update([outputs])
+        sim.draw()
+
+        if sim.walkers[0].info().headAltitude < 0.4:
+            print("Walker fell down, stopping simulation.")
+            break
